0ab6a94186d61494fe635b00c90ff47a6e33785f
Traii Mohamed Amin <99541159+themedworld@users.noreply.github.com>
2025-05-24 12:15:35 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
     credentials: true,
   }));
   app.setGlobalPrefix('api/v1')
-  await app.listen(3001);
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
 }
 bootstrap();
