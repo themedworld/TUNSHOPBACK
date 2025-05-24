9e1aa1c32f5c128c31cc29facf534dfdfb15b5f7
Traii Mohamed Amin <99541159+themedworld@users.noreply.github.com>
2025-05-24 16:57:13 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
   app.use(cors({
     origin: ['http://localhost:3000',
-             'https://tunshop-v86p-git-main-themedworlds-projects.vercel.app',
+             'https://tunshop-v86p.vercel.app',
               ],
     credentials: true,
   }));
